1114742c5e38489ec28f196bdcd140baba3efbab
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2024-12-08 06:32:00 +0300

    0.7.1.772 support

--- Code/main.cpp
+++ Code/main.cpp
@@ -39,11 +39,11 @@ static bool g_mhAttached = false;
 
 void dll_attach()
 {
-	if (!SmSdk::CheckTimestamp(_SM_TIMESTAMP_070_771))
+	if (!SmSdk::CheckTimestamp(_SM_TIMESTAMP_071_772))
 	{
 		MessageBoxA(
 			NULL,
-			"Your game version is unsupposed by Custom Audio Extension. The current version of the mod has been built for Scrap Mechanic 0.7.0.771",
+			"Your game version is not supported by Custom Audio Extension. The current version of the mod has been built for Scrap Mechanic 0.7.1.772\n\nPress OK to continue loading without the mod.",
 			"Unsupported Version",
 			MB_ICONWARNING);
 		return;
